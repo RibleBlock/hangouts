733bd7a11bd72a876874db81fb76d7a8460ec448
Riquelme <riquelmeaparecido08@gmail.com>
2022-05-31 23:40:37 -0300

    Pagina de login...

---
+++ frontend/src/pages/Login/Login.styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Div = styled.div`
+  height: 50rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  > form {
+    width: 100%;
+    max-width: 45rem;
+    padding: 3.3rem;
+    margin-bottom: 8rem;
+    font-size: 1.6rem;
+    font-weight: bold;
+    border-radius: 1.3rem;
+    background-color: #E8E8E8;
+
+    > p {
+      margin-bottom: .7rem;
+    }
+    > input {
+      width: 100%;
+      padding: 1rem 1rem;
+      margin-bottom: 2rem;
+      font-size: 1.6rem;
+      border-radius: 1rem;
+    }
+  }
+`;
--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,5 +1,21 @@
+import { Footer, Header, NavigationBar } from '../../components';
+import { Div } from './Login.styles';
+
 export function Login() {
   return (
-    <h1>LOGIN</h1>
+    <>
+      <NavigationBar />
+      <Header title="Login" />
+      <Div>
+        <form action="" method="post">
+          <p>Nome:</p>
+          <input type="text" />
+          <p>Senha:</p>
+          <input type="text" />
+          <button type="submit">Entrar</button>
+        </form>
+      </Div>
+      <Footer />
+    </>
   );
 }
