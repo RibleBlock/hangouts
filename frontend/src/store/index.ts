5ce4d3635f9a68e834a10f8eadbd3cfa5556a106
Riquelme <riquelmeaparecido08@gmail.com>
2022-06-08 09:58:54 -0300

    reducer user

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -2,9 +2,11 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { State } from './module';
 
 import pedidoState from './Pedido/reducer';
+import userState from './User/reducer';
 
 const reducer = combineReducers<State>({
   cart: pedidoState,
+  user: userState,
 });
 
 const store = configureStore({ reducer });
--- frontend/src/store/module.d.ts
+++ frontend/src/store/module.d.ts
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-vars */
 import { Cart } from './Pedido/reducer';
+import { User } from './User/reducer';
 
 interface State {
   cart: Cart;
+  user: User;
 }
