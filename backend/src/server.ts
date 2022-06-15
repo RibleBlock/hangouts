81240680f333f55990031a3673c06d3a0d93a7a9
Riquelme <riquelmeaparecido08@gmail.com>
2022-06-14 23:26:16 -0300

    cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
+
+app.use(cors({ origin: 'http://localhost:3000' }));
 const port = 3333;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express);
 app.use(routes);
 
 app.listen(port, () => console.log(`Server iniciado em http://localhost:${port}`));
