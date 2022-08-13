0df3f19174d47d71bf2d9a23bddfd84baa28b801
Riquelme <riquelmeaparecido08@gmail.com>
2022-08-12 21:29:57 -0300

    usando mult-repo

--- backend/src/config/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-require('dotenv').config();
-
-const supabaseUrl = <string>process.env.SUPABASE_URL;
-const supabaseKey = <string>process.env.SUPABASE_KEY;
-export default createClient(supabaseUrl, supabaseKey);
--- backend/src/utils/checkPassword.util.ts
+++
@@ -1,5 +0,0 @@
-import bcryptjs from 'bcryptjs';
-
-export async function passwordIsValid(password: string, passwordHash: string) {
-  return bcryptjs.compare(password, passwordHash);
-}
